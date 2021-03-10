f1ea72700295e93af87f66f3550a36d0d16a26d1
D·Teach <1114626450@qq.com>
2021-03-10 15:23:49 +0800

    导入相关实体类

---
+++ service/service-hosp/src/main/java/com/dlq/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,18 @@
+package com.dlq.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@program: common-util
+ *@description:
+ *@author: Hasee
+ *@create: 2021-03-10 14:59
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class,args);
+    }
+}
