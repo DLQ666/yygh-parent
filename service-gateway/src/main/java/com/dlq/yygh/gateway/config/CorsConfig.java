b815dc87f3702c05288a555e3434cd9b21bfda61
D·Teach <1114626450@qq.com>
2021-03-14 22:27:14 +0800

    配置网关跨域

---
+++ service-gateway/src/main/java/com/dlq/yygh/gateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.dlq.yygh.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @program: learn_parent
+ * @description: 网关跨域配置类
+ * @author: Hasee
+ * @create: 2020-07-07 20:48
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
