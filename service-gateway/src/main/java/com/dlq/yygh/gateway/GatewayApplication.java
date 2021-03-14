d21eac86d4c3e5a3bd088d94485cf827b1ad17a0
D·Teach <1114626450@qq.com>
2021-03-14 17:53:34 +0800

    添加Gateway网关

---
+++ service-gateway/src/main/java/com/dlq/yygh/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.dlq.yygh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@program: yygh-parent
+ *@description:
+ *@author: Hasee
+ *@create: 2021-03-14 17:28
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
