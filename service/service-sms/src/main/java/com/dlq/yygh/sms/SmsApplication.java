6001e250375e523d5bd9210b43342ed0e2495de7
D·Teach <1114626450@qq.com>
2021-03-15 19:42:05 +0800

    初始化阿里云短信微服务

---
+++ service/service-sms/src/main/java/com/dlq/yygh/sms/SmsApplication.java
@@ -0,0 +1,17 @@
+package com.dlq.yygh.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *@program: yygh-parent
+ *@description:
+ *@author: Hasee
+ *@create: 2021-03-15 19:37
+ */
+@SpringBootApplication
+public class SmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class, args);
+    }
+}
