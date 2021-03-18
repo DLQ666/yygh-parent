459b264a3d48f5ed816feaf5a0c0c9a9c0d272fd
D·Teach <1114626450@qq.com>
2021-03-18 20:15:27 +0800

    修复短信微服务Bug

--- service/service-sms/src/main/java/com/dlq/yygh/sms/SmsApplication.java
+++ service/service-sms/src/main/java/com/dlq/yygh/sms/SmsApplication.java
@@ -1,5 +1,6 @@
 package com.dlq.yygh.sms;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  *@author: Hasee
  *@create: 2021-03-15 19:37
  */
+@EnableRabbit
 @ComponentScan("com.dlq.yygh")
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//取消数据源自动配置
 public class SmsApplication {
